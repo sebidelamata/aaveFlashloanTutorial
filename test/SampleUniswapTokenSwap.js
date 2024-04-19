658fea6cd5a175fedd7d480e73b7ab4bd8a6a5e6
Miguel  de la Mata <sebidelamata@gmail.com>
2024-04-19 17:51:06 -0600

    send WETH to swap contract

--- test/SampleUniswapTokenSwap.js
+++ test/SampleUniswapTokenSwap.js
@@ -64,28 +64,55 @@ describe("SampleUniswapTokenSwap", function () {
 
     expect(boopPrice).not.equals(null)
   })
-  // it("Should be able to swap WETH to BOOP", async function(){
-  //   // Contracts are deployed using the first signer/account by default
-  //   const owner = await ethers.getSigners();
-  //   const wethABI = wethArtifact.abi
-  //   const wethAddress = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
-  //   wethContract = new ethers.Contract(
-  //     wethAddress,
-  //     wethABI,
-  //     signer[0]
-  //   )
+  it("Should be able to swap WETH to BOOP", async function(){
+    // Contracts are deployed using the first signer/account by default
+    const signer = await ethers.getSigners();
+    const wethABI = wethArtifact.abi
+    const wethAddress = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+    wethContract = new ethers.Contract(
+      wethAddress,
+      wethABI,
+      signer[0]
+    )
 
-  //   let wethBalance = await wethContract.balanceOf(signer[0])
-  //   console.log("WETH balance: " + wethBalance)
+    let wethBalance = await wethContract.balanceOf(signer[0])
+    console.log("Signer WETH balance: " + wethBalance)
     
-  //   const SampleUniswapTokenSwap = await ethers.getContractFactory("SampleUniswapTokenSwap");
-  //   const sampleUniswapTokenSwap = await SampleUniswapTokenSwap.deploy();
-  //   await sampleUniswapTokenSwap.waitForDeployment();
-  //   const contractAddress = await sampleUniswapTokenSwap.getAddress()
-  //   console.log("Swap Contract address: " + contractAddress)
+    // deploy uniswap contract
+    const SampleUniswapTokenSwap = await ethers.getContractFactory("SampleUniswapTokenSwap");
+    const sampleUniswapTokenSwap = await SampleUniswapTokenSwap.deploy();
+    await sampleUniswapTokenSwap.waitForDeployment();
+    const contractAddress = await sampleUniswapTokenSwap.getAddress()
+    console.log("Swap Contract address: " + contractAddress)
 
-    
+    // send WETH to uniswap contract
+    let sendAmount = ethers.parseEther('1')
+    let tx = await wethContract.connect(signer[0]).transfer(contractAddress, sendAmount)
+    await tx.wait()
 
-  //   expect(wethBalance).to.equal(ethers.parseEther('1'))
-  // })
+    // check contract WETH balance
+    let contractBalance = await wethContract.balanceOf(contractAddress)
+    console.log('Swap Contract WETH Balance: ', contractBalance.toString())
+
+    // get price of boop to calculate minOut
+    const boopWETHPoolAddress = '0xe24F62341D84D11078188d83cA3be118193D6389'
+    const uniswapV3PoolABI = uniswapV3PoolArtifact.abi
+    const boopWETHPool = new ethers.Contract(
+      boopWETHPoolAddress, 
+      uniswapV3PoolABI, 
+      owner[0]
+    );
+    const slot0 = await boopWETHPool.slot0()
+    const sqrtPriceX96 = slot0.sqrtPriceX96.toString()
+    console.log(sqrtPriceX96.toString())
+    const boopPrice = ((sqrtPriceX96 / 2**96)**2) / (10**18 / 10**18).toFixed(18);
+    console.log(boopPrice)
+
+    // calculate slippage
+
+    // swap tokens
+
+
+    expect(contractBalance).not.equals(0)
+  })
 })
