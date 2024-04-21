8d16ef09dc9da319199fc57a9ca3b0502bb74501
Miguel  de la Mata <sebidelamata@gmail.com>
2024-04-20 22:17:30 -0600

    boop cheap uniswap inputs ready

--- test/FlashLoanBoopCheapUniswap.js
+++ test/FlashLoanBoopCheapUniswap.js
@@ -67,87 +67,90 @@ describe("FlashLoanBoopCheapUniswap", function () {
 
     expect(amountOut[1].toString()).not.equals(null)
   })
-  // it("Should be able to swap WETH to BOOP", async function(){
-  //   // Contracts are deployed using the first signer/account by default
-  //   const signer = await ethers.getSigners();
-  //   const wethABI = wethArtifact.abi
-  //   const wethAddress = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
-  //   const wethContract = new ethers.Contract(
-  //     wethAddress,
-  //     wethABI,
-  //     signer[0]
-  //   )
+  it("Should be able to perform BOOP cheap uniswap arbitrage operation", async function(){
+    // set up weth get starting balance of signer
+    const signer = await ethers.getSigners();
+    const wethABI = wethArtifact.abi
+    const wethAddress = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+    wethContract = new ethers.Contract(
+      wethAddress,
+      wethABI,
+      signer[0]
+    )
 
-  //   let wethBalance = await wethContract.balanceOf(signer[0])
-  //   console.log("Signer WETH balance: " + wethBalance)
-  //   // deposit 1 eth
-  //   const input = ethers.parseEther('1')
-  //   let tx = await wethContract.connect(signer[0]).deposit({ value: input })
-  //   await tx.wait()
+    let wethBalance = await wethContract.balanceOf(signer[0])
+    console.log("Signer WETH balance: " + wethBalance)
+    // deposit 1 eth
+    const input = ethers.parseEther('1')
+    let tx = await wethContract.connect(signer[0]).deposit({ value: input })
+    await tx.wait()
 
-  //   // grab balance of WETH
-  //   wethBalance = await wethContract.balanceOf(signer[0])
-  //   console.log("Signer WETH Balance" + wethBalance)
+    // deploy arbitrage contract
+    const FlashLoanBoopCheapUniswap = await ethers.getContractFactory("FlashLoanBoopCheapUniswap");
+    const flashLoanBoopCheapUniswap = await FlashLoanBoopCheapUniswap.deploy('0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb');
+    await flashLoanBoopCheapUniswap.waitForDeployment();
+    const contractAddress = await flashLoanBoopCheapUniswap.getAddress()
+    console.log(contractAddress)
+    console.log("Boop Cheap Uniswap Arbitrage Contract address: " + contractAddress)
+
+    // get uniswap inputs
+
+    // send WETH to uniswap contract
+    let sendAmount = ethers.parseEther('1')
+    tx = await wethContract.connect(signer[0]).transfer(contractAddress, sendAmount)
+    await tx.wait()
+
+    // check contract WETH balance
+    let contractBalance = await wethContract.balanceOf(contractAddress)
+    console.log('Swap Contract WETH Balance: ', contractBalance.toString())
+
+    // get price of boop to calculate minOut
+    const boopWETHPoolAddress = '0xe24F62341D84D11078188d83cA3be118193D6389'
+    const uniswapV3PoolABI = uniswapV3PoolArtifact.abi
+    const boopWETHPool = new ethers.Contract(
+      boopWETHPoolAddress, 
+      uniswapV3PoolABI, 
+      owner[0]
+    );
+    const slot0 = await boopWETHPool.slot0()
+    const sqrtPriceX96 = slot0.sqrtPriceX96.toString()
+    console.log("sqrtPriceX96: " + sqrtPriceX96)
+    const uniswapBoopPrice = ((sqrtPriceX96 / 2**96)**2) / (10**18 / 10**18).toFixed(18);
+    console.log("Uniswap Boop Price: " + uniswapBoopPrice)
+    // params, calculate slippage
+    const uniswapAmountIn = ethers.parseEther('1').toString()
+    console.log("Uniswap WETH amountIn: " + uniswapAmountIn)
+    // allow 5% slippage
+    const uniswapAmountOutMin = BigInt(((1 / uniswapBoopPrice) * 0.95) * 10**18)
+    console.log("Uniswap BOOP amountOutMin: " + uniswapAmountOutMin)
+
+    // get camelot constants
+
+    // params, calculate slippage
+    const camelotAmountIn = uniswapAmountOutMin
+    console.log("Camelot min amountIn: " + camelotAmountIn)
+    // allow 5% slippage
+    // connect to router
+    const routerAddress = '0xc873fEcbd354f5A56E00E710B90EF4201db2448d'
+    const camelotABI = camelotSwapRouterArtifact.abi
+    const camelotSwapRouterContract = new ethers.Contract(
+      routerAddress, 
+      camelotABI, 
+      owner[0]
+    );
+    // get amount out
+    let camelotAmountOutMin = await camelotSwapRouterContract.getAmountsOut(
+      camelotAmountIn,
+      [
+        '0x13A7DeDb7169a17bE92B0E3C7C2315B46f4772B3',
+        '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+      ]
+    )
+    camelotAmountOutMin = (camelotAmountOutMin[1] * BigInt(95)) / BigInt(100)
+    camelotAmountOutMin = camelotAmountOutMin.toString()
+    console.log("Camelot WETH amountOutMin: " + camelotAmountOutMin)
+
+    // perform arbitrage operation
     
-  //   // deploy camelot swap contract
-  //   const SampleCamelotSwap = await ethers.getContractFactory("SampleCamelotSwap");
-  //   const sampleCamelotSwap = await SampleCamelotSwap.deploy();
-  //   await sampleCamelotSwap.waitForDeployment();
-  //   const contractAddress = await sampleCamelotSwap.getAddress()
-  //   console.log("Swap Contract address: " + contractAddress)
-
-  //   // send WETH to camelot swap contract
-  //   let sendAmount = ethers.parseEther('1')
-  //   tx = await wethContract.connect(signer[0]).transfer(contractAddress, sendAmount)
-  //   await tx.wait()
-
-  //   // check contract WETH balance
-  //   let contractBalance = await wethContract.balanceOf(contractAddress)
-  //   console.log('Swap Contract WETH Balance: ', contractBalance.toString())
-
-  //   // params, calculate slippage
-  //   const amountIn = ethers.parseEther('1').toString()
-  //   console.log("amountIn: " + amountIn)
-  //   // allow 5% slippage
-  //   // connect to router
-  //   const routerAddress = '0xc873fEcbd354f5A56E00E710B90EF4201db2448d'
-  //   const camelotABI = camelotSwapRouterArtifact.abi
-  //   const camelotSwapRouterContract = new ethers.Contract(
-  //     routerAddress, 
-  //     camelotABI, 
-  //     owner[0]
-  //   );
-
-  //   // get amount out
-  //   let amountOutMin = await camelotSwapRouterContract.getAmountsOut(
-  //     amountIn,
-  //     [
-  //       '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-  //       '0x13A7DeDb7169a17bE92B0E3C7C2315B46f4772B3'
-  //     ]
-  //   )
-  //   amountOutMin = (amountOutMin[1] * BigInt(95)) / BigInt(100)
-  //   amountOutMin = amountOutMin.toString()
-  //   console.log("amountOutMin: " + amountOutMin)
-  //   // swap tokens
-  //   tx = await sampleCamelotSwap.connect(signer[0]).swapExactInputSingle(
-  //     amountIn,
-  //     amountOutMin
-  //   )
-  //   await tx.wait()
-    
-  //   // find contract balance of Boop
-  //   const boopABI = wethArtifact.abi
-  //   const boopAddress = '0x13A7DeDb7169a17bE92B0E3C7C2315B46f4772B3'
-  //   boopContract = new ethers.Contract(
-  //     boopAddress,
-  //     boopABI,
-  //     signer[0]
-  //   )
-  //   let boopBalance = await boopContract.balanceOf(contractAddress)
-  //   console.log('Swap Contract Boop Balance: ', boopBalance.toString())
-
-
-  //   expect(boopBalance).not.equals(0)
-  // })
+  })
 })
